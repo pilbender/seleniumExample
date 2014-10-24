4afa51216318d22dacf8dd49acc4ed3f86c710cf
Richard Scott Smith <scott.smith@isostech.com>
2014-10-23 21:04:09 -0700

    Added a Dto marker interface.  Might as well follow some good design practices for the example.

---
+++ src/main/java/net/raescott/seleniumexample/dto/Dto.java
@@ -0,0 +1,15 @@
+package net.raescott.seleniumexample.dto;
+
+import java.io.Serializable;
+
+/**
+ * Marker Interface for DTOs, Data Transfer Objects, also known as Value Objects.
+ *
+ * DTO's are business domain objects that are passed through the layers of the
+ * architecture.
+ *
+ * @author Richard Scott Smith <scott.smith@isostech.com>
+ */
+public interface Dto extends Serializable {
+
+}
