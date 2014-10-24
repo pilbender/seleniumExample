2776bc0ae496490ddb19f947df32a4be876f8587
Richard Scott Smith <scott.smith@isostech.com>
2014-10-23 21:27:46 -0700

    Added a UserDto for carrying user information.  This will be used by the login form.

---
+++ src/main/java/net/raescott/seleniumexample/dto/UserDto.java
@@ -0,0 +1,93 @@
+package net.raescott.seleniumexample.dto;
+
+/**
+ * Data Transfer object for carrying user information.
+ *
+ * @author Richard Scott Smith <scott.smith@isostech.com>
+ */
+public class UserDto implements Dto {
+	private String email;
+	private String firstName;
+	private String lastName;
+	private String password;
+	private int userId;
+	private boolean rememberMe;
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the userId
+	 */
+	public int getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public boolean isRememberMe() {
+		return rememberMe;
+	}
+
+	public void setRememberMe(boolean rememberMe) {
+		this.rememberMe = rememberMe;
+	}
+}
